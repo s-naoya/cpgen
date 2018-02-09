bba41745e7f70fb94ab75709c4c581e276e45e46
Saito Naoya@cc06_Ubuntu_12.04 <saito@scc.ist.hokudai.ac.jp>
2018-02-09 15:04:41 +0900

    add PlanFootprints class

---
+++ plan_footprints.cpp
@@ -0,0 +1,7 @@
+#include "plan_footprints.h"
+
+namespace cp{
+
+
+
+}; // namespace cp
---
+++ plan_footprints.h
@@ -0,0 +1,22 @@
+#ifndef CPGEN_PLAN_FOOTPRINTS_H_
+#define CPGEN_PLAN_FOOTPRINTS_H_
+
+#include <iostream>
+#include <cmath>
+
+#include "eigen_types.h"
+
+namespace cp {
+
+class PlanFootprints {
+ public:
+  PlanFootprints() {}
+  ~PlanFootprints() {}
+
+ private:
+
+};
+
+}  // namespace cp
+
+#endif  // CPGEN_PLAN_FOOTPRINTS_H_
